525605f04d12000f9f8ffaac76766347490ba990
Gajus Kuizinas <gajus@gajus.com>
2016-08-22 18:42:54 +0100

    fix: use standard-release

---
+++ release.sh
@@ -0,0 +1,10 @@
+rm -fr ./node_modules
+git config --global user.name 'standard-version'
+git config --global user.email 'standard-version@travis'
+git checkout master
+git pull origin master
+npm install
+NODE_ENV=production npm run build
+standard-version
+git push --follow-tags origin master
+npm publish
