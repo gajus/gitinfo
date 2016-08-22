e10afc6f076fe5cac5d97559161b9d0b8bfacb18
Gajus Kuizinas <gajus@gajus.com>
2016-08-23 00:27:43 +0100

    fix: correct release.sh script

--- release.sh
+++ release.sh
@@ -7,10 +7,10 @@ if [[ $TRAVIS_PULL_REQUEST = "true" ]]; then
   exit 0
 fi
 
-echo '$TRAVIS_TAG';
-echo $TRAVIS_TAG;
-
 if [[ $TRAVIS_TAG != "" ]]; then
+    # Use NPM_TOKEN to enable NPM authentication
+    echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+
     NODE_ENV=development npm install --depth 0
     NODE_ENV=production npm run build
 
@@ -30,9 +30,6 @@ git config --global user.email 'standard-version@travis'
 # Use GITHUB_TOKEN to enable GitHub authentication
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
-
-# Use NPM_TOKEN to enable NPM authentication
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
 
 git checkout master
 git merge $TRAVIS_COMMIT
