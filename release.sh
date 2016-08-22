04fd5d3d4b02804e38abbf6f2b798fec74e54add
Gajus Kuizinas <gajus@gajus.com>
2016-08-22 19:48:52 +0100

    fix: correct release.sh script

--- release.sh
+++ release.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -ev
 rm -fr ./node_modules
-# git config --global user.name 'standard-version'
-# git config --global user.email 'standard-version@travis'
+git config --global user.name 'standard-version'
+git config --global user.email 'standard-version@travis'
 
 echo $TRAVIS_BRANCH
 echo $TRAVIS_COMMIT_RANGE
 echo $TRAVIS_EVENT_TYPE
 echo $TRAVIS_REPO_SLUG
 echo $TRAVIS_PULL_REQUEST
+
+git remote -v
 
 git checkout master
 git pull origin master
