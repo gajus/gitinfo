8476abdb38517970ffadb70616afbf1f09e142ac
Gajus Kuizinas <gajus@gajus.com>
2016-08-22 19:24:49 +0100

    fix: correct release.sh script

--- release.sh
+++ release.sh
@@ -5,8 +5,10 @@ git config --global user.name 'standard-version'
 git config --global user.email 'standard-version@travis'
 git checkout master
 git pull origin master
-npm install
+npm install --depth 0
 NODE_ENV=production npm run build
 standard-version
+echo $GITHUB_TOKEN
+echo $NPM_TOKEN
 git push --follow-tags origin master
 npm publish
