5c9a2f048f9f53ff0c386e273d2839c7e3fc9635
Gajus Kuizinas <gajus@gajus.com>
2016-08-22 19:42:20 +0100

    fix: correct release.sh script

--- release.sh
+++ release.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -ev
 rm -fr ./node_modules
-git config --global user.name 'standard-version'
-git config --global user.email 'standard-version@travis'
+# git config --global user.name 'standard-version'
+# git config --global user.email 'standard-version@travis'
+
+echo $TRAVIS_BRANCH
+echo $TRAVIS_COMMIT_RANGE
+echo $TRAVIS_EVENT_TYPE
+echo $TRAVIS_REPO_SLUG
+echo $TRAVIS_PULL_REQUEST
+
 git checkout master
 git pull origin master
-npm install --depth 0
+NODE_ENV=development npm install --depth 0
 NODE_ENV=production npm run build
 standard-version
-echo $GITHUB_TOKEN
-echo $NPM_TOKEN
 git push --follow-tags origin master
 npm publish
