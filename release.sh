052ae0ce47185f1f5184b9a82f17b0c46e9ea29b
Gajus Kuizinas <gajus@gajus.com>
2016-08-23 08:47:03 +0100

    fix: correct release.sh script

---
+++ release.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -ev
+
+if [[ $TRAVIS_PULL_REQUEST = "true" ]]; then
+  echo 'this is PR, exiting'
+
+  exit 0
+fi
+
+if [[ $TRAVIS_TAG != "" ]]; then
+    # Use NPM_TOKEN to enable NPM authentication
+    echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+
+    NODE_ENV=development npm install --depth 0
+    NODE_ENV=production npm run build
+
+    npm publish
+    exit 0
+fi
+
+if [[ $(git log --format=%B -n 1 $TRAVIS_COMMIT) == *"chore: release"* ]]; then
+    echo 'this is a release, exiting'
+
+    exit 0
+fi;
+
+git config --global user.name 'standard-version'
+git config --global user.email 'standard-version@travis'
+
+# Use GITHUB_TOKEN to enable GitHub authentication
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
+
+git checkout master
+git merge $TRAVIS_COMMIT
+
+standard-version --message "chore: release %s"
+
+npm run documentation
+
+git add ./README.md
+git commit --amend --no-edit
+
+git push --follow-tags origin master
